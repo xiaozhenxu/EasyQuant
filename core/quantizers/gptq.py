2ed8e715d468b90bab0ca0e3e081a1c9a5da36d1
xiaozhenxu <1120839635@qq.com>
2025-09-10 23:23:23 +0800

    gptq call func

--- core/quantizers/gptq.py
+++ core/quantizers/gptq.py
@@ -25,7 +25,7 @@ class GPTQ:
         input: torch.Tensor,
         output: torch.Tensor
     ):
-        pass
+        print("-----")
 
     def quantize(
         self
@@ -56,17 +56,69 @@ def find_layers(module: nn.Module, name: str = "", layers=[nn.Linear]) -> {str, 
     
     return res
 
-def run_gptq_for_sequenial_layers(module: nn.ModuleList):
+def run_gptq_for_sequenial_layers(module: nn.ModuleList, input_data: torch.Tensor, gptq_config: GPTQConfig):
+    gptqs: dict[str, GPTQ] = dict()
+    batch_size, seq_length, hidden_size = input_data.shape
+
+    # create position_ids and attention_mask
+    position_ids = torch.arange(seq_length, dtype=torch.long)
+    position_ids = position_ids.unsqueeze(0).expand(batch_size, -1)
+
+    '''
+    shape: [batch_size, 1, seq_length, seq_length]
+    下三角矩阵，防止看到未来信息
+    attention_mask = [[[[0, -10000, -10000, -10000],
+                        [0, 0, -10000, -10000],
+                        [0, 0, 0, -10000],
+                        [0, 0, 0, 0]]]]
+    '''
+    attention_mask = torch.ones((batch_size, seq_length))
+    attention_mask = attention_mask.reshape(batch_size, -1, -1, seq_length)
+
+    device = input_data.device()
+    position_ids = position_ids.to(device)
+    attention_mask = attention_mask.to(device)
+
     for i in range(len(module)):
         # print(type(module[i]))
-        sub_layers_dict = find_layers(module[i])
+        decoder_layer = module[i]
+        decoder_layer.eval()
+
+        sub_layers_dict = find_layers(decoder_layer)
         sub_layers_dict = {name: module for name, module in sub_layers_dict.items() if is_legal_linear(module)}
-        for name, module in sub_layers_dict.items():
-            print(name, module)
-        break
+
+        # add gptq for each linear
+        for name, layer in sub_layers_dict.items():
+            gptqs[name] = GPTQ(layer, gptq_config)
+        
+        # add forward hooks for each linear
+        handles = []
+        def add_batch(module_name):
+            def add_batch_fn(self, input, output):
+                assert len(input) == 1, "shoule have one input tensor"
+                gptqs[module_name].add_batch(input[0], output)
+            return add_batch_fn
+        for name, layer in sub_layers_dict.items():
+            handles.append(layer.register_forward_hook(add_batch(name)))
+
+        # iterate data to record stats for gptq
+        next_input_tensor = []
+        input_tensor = [input_data[0] for n in range(batch_size)]
+        for j in range(batch_size):
+            output = decoder_layer(
+                hidden_states=input_tensor[j].unsqueeze(0),
+                attention_mask=attention_mask[j:j+1],
+                position_ids=position_ids[j:j+1],
+                output_attentions=False
+            )
+            next_input_tensor.append(output)
+        input = next_input_tensor
+
 
 if __name__ == "__main__":
     MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
     qwen_model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
-    run_gptq_for_sequenial_layers(qwen_model.model.layers)
-    # print(qwen_model)
+    gptq_config = GPTQConfig()
+    data = torch.rand((64, 16, 896))
+    run_gptq_for_sequenial_layers(qwen_model.model.layers, data, gptq_config)
+    print(qwen_model)
